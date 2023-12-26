bd03fdcaa81bf28f0064e93904aaeb8df49983c2
Prakhar singh <singhprakhar32@gmail.com>
2023-12-26 23:53:18 +0530

    started adding basic setup with toolkit

--- README.md
+++ README.md
@@ -2,22 +2,22 @@
 
 ## Setup Instructions
 1. Clone the project
-``
+```
     git clone https://github.com/singhprakhar32/task-rev1.git
-``
+``````
 2. Move into the directory
-``
+```
     cd task-rev1 
     cd task-management
-``
+```
 3. install dependencies
-``
+```
     npm i
-``
+```
 4. run the server
-``
+```
     npm run start
-``
+```
 ### Setup instructions for bootstrap
 ```
     npm install react-bootstrap bootstrap
--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 import './index.css';
 import App from './App';
+import store from './Redux/store';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from 'react-hot-toast';
+import { Provider } from 'react-redux';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <Provider store={store}>
   <BrowserRouter>
      <App />
      <Toaster/>
   </BrowserRouter>
-   
+  </Provider>   
 );
 
 // If you want to start measuring performance in your app, pass a function
